452d15f48ae1364f42992aa98e301dc2b47aa70c
Meat Demiurg <150423035+Nekro-Demiurg@users.noreply.github.com>
2024-12-29 19:13:39 +0400

    Update 01_web.user.js

--- 01_web.user.js
+++ 01_web.user.js
@@ -1,33 +1,21 @@
 (function() {
     'use strict';
-    
- 
-    const modernUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1";
-    
 
-    Object.defineProperties(navigator, {
-        userAgent: {
-            get: () => modernUserAgent
-        },
-        platform: {
-            get: () => 'iPhone'
-        },
-        vendor: {
-            get: () => 'Apple Computer, Inc.'
-        },
-        maxTouchPoints: {
-            get: () => 5
-        }
+    var modernUserAgent  = "Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1";
+
+    Object.defineProperty(navigator, 'userAgent', {
+        get: function() { return modernUserAgent; }
     });
 
+    Object.defineProperty(navigator, 'platform', {
+        get: function() { return 'iPhone'; }
+    });
 
+    Object.defineProperty(navigator, 'vendor', {
+        get: function() { return 'Apple Computer, Inc.'; }
+    });
     function emitTouchEvent(event, touchType) {
-        if (typeof Touch === 'undefined' || typeof TouchEvent === 'undefined') {
-            console.warn('Touch API не поддерживается в этом браузере');
-            return;
-        }
-
-        const touchObj = new Touch({
+        var touchObj = new Touch({
             identifier: Date.now(),
             target: event.target,
             clientX: event.clientX,
@@ -39,38 +27,31 @@
             radiusX: 2.5,
             radiusY: 2.5,
             rotationAngle: 10,
-            force: 1.0,
+            force: 0.5,
         });
 
-        const touchEvent = new TouchEvent(touchType, {
+        var touchEvent = new TouchEvent(touchType, {
             cancelable: true,
             bubbles: true,
-            composed: true,
-            touches: touchType !== 'touchend' ? [touchObj] : [],
-            targetTouches: touchType !== 'touchend' ? [touchObj] : [],
-            changedTouches: [touchObj]
+            shiftKey: true,
+            touches: [touchObj],
+            targetTouches: [touchObj],
+            changedTouches: [touchObj],
         });
 
         event.target.dispatchEvent(touchEvent);
-        
-        
-        event.preventDefault();
     }
 
-   
-    const handleMouseDown = (event) => emitTouchEvent(event, 'touchstart');
-    const handleMouseMove = (event) => emitTouchEvent(event, 'touchmove');
-    const handleMouseUp = (event) => emitTouchEvent(event, 'touchend');
+    document.addEventListener('mousedown', function(event) {
+        emitTouchEvent(event, 'touchstart');
+    });
 
-    
-    document.addEventListener('mousedown', handleMouseDown, { passive: false });
-    document.addEventListener('mousemove', handleMouseMove, { passive: false });
-    document.addEventListener('mouseup', handleMouseUp, { passive: false });
+    document.addEventListener('mousemove', function(event) {
+        emitTouchEvent(event, 'touchmove');
+    });
 
-   
-    window.clearTouchEmulation = () => {
-        document.removeEventListener('mousedown', handleMouseDown);
-        document.removeEventListener('mousemove', handleMouseMove);
-        document.removeEventListener('mouseup', handleMouseUp);
-    };
+    document.addEventListener('mouseup', function(event) {
+        emitTouchEvent(event, 'touchend');
+    });
+
 })();
