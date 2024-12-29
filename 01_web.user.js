7ffa1288602cf81b6f1a784a58ccb2f82c043006
Meat Demiurg <150423035+Nekro-Demiurg@users.noreply.github.com>
2024-12-29 19:17:14 +0400

    Update 01_web.user.js

--- 01_web.user.js
+++ 01_web.user.js
@@ -1,10 +1,10 @@
 (function() {
     'use strict';
 
-    var modernUserAgent  = "Mozilla/5.0 (iPhone; CPU iPhone OS 15_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Mobile/15E148 Safari/604.1";
+    var newUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1";
 
     Object.defineProperty(navigator, 'userAgent', {
-        get: function() { return modernUserAgent; }
+        get: function() { return newUserAgent; }
     });
 
     Object.defineProperty(navigator, 'platform', {
